da6f4662d61af105457a7ff3ec25bf2261defe7e
Nobody <mishasikolenko@mail.ru>
2024-02-26 20:51:04 +0300

    Lab1_1

---
+++ Lab1/lab1_1.R
@@ -0,0 +1,25 @@
+library(plotly)
+
+# Постройте график функции и её производных $F=(x-5)^2+6x$. Найдите минимум функции.
+
+F <- function(x){
+  (x - 5) ^ 2 + 6 * x
+}
+
+dF <- function(x){
+  2 * (x - 5) + 6
+}
+
+x <- seq(-5, 10, by = 0.1)
+y <- F(x)
+dy <- dF(x)
+data <- data.frame(x, y)
+Ddata <- data.frame(x, dy)
+fig <- plot_ly(data, x = ~x, y=~y,type = 'scatter', mode = 'lines') %>%
+  add_trace(Ddata, x = ~x, y = ~dy,type = 'scatter', mode = 'lines')
+
+fig
+
+op <- optimise(F, c(-5, 10))
+cat(op$objective)
+cat(dF(op$minimum))
