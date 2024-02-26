5c89c7bcbaf6f63e935d49b7e65f076f79074f20
Nobody <mishasikolenko@mail.ru>
2024-02-26 22:18:15 +0300

    Lab1_3

---
+++ Lab1/lab1_3.R
@@ -0,0 +1,35 @@
+library(plotly)
+
+# Постройте объемный и контурный график функции $F=3x^2+xy+2y^2-x-4y$. 
+# Найдите частные производные. Определите точки максимума, минимума и перегиба.
+
+F <- function(x, y){
+  3 * x^2 + x * y + 2 * y ^ 2 - x - 4 * y
+}
+
+dFdx <- function(x, y){
+  6 * x + y - 1 
+}
+
+dFdy <- function(x, y){
+  x + 4 * y - 4
+}
+
+d2Fdx2 <- function(x, y){
+  6
+}
+
+d2Fdy2 <- function(x, y){
+  4
+}
+
+d2Fdxdy <- function(x, y){
+  1
+}
+
+x <- y <- seq(-5, 5, length.out = 100)
+z <- outer(x, y, FUN = F)
+plot_ly(z = ~z) |> add_surface()
+
+z <- outer(x, y, FUN = F)
+plot_ly(x = ~x, y = ~y, z = ~z, type = "contour")
